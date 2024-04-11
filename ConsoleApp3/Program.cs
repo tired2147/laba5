dc2b94ccaac0d816120a229e56453db7794dca1a
OLEG <olegemail@mail.ru>
2024-04-11 22:39:29 +0300

    Добавлено возведение в степень

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Введите а:");
 double a = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите b:");
 double b = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите знак (+, -, *, /):");
+Console.WriteLine("Введите знак (+, -, *, /, **):");
 string znak = Console.ReadLine();
 
 switch (znak)
@@ -18,7 +18,16 @@ switch (znak)
         Console.WriteLine("Ответ: " + (a * b));
         break;
     case "/":
-        Console.WriteLine("Ответ: " + (a / b));
+        if (b == 0)
+        {
+            Console.WriteLine("На 0 нельзя!");
+        }
+        else
+        {
+            Console.WriteLine("Ответ: " + (a / b));
+        }
         break;
-
+    case "**":
+        Console.WriteLine("Ответ: " + (Math.Pow(a, b)));
+        break;
 }
