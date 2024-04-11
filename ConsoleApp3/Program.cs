cff95b148e9d9d11ae0d3252c376a199aa7ddfe9
OLEG <olegemail@mail.ru>
2024-04-11 21:48:17 +0300

    1-ая версия калькулятора

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Введите а:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите знак (+, -, *, /):");
+string znak = Console.ReadLine();
+
+switch (znak)
+{
+    case "+":
+        Console.WriteLine("Ответ: " + (a + b));
+        break;
+    case "-":
+        Console.WriteLine("Ответ: " + (a - b));
+        break;
+    case "*":
+        Console.WriteLine("Ответ: " + (a * b));
+        break;
+    case "/":
+        Console.WriteLine("Ответ: " + (a / b));
+        break;
+
+}
